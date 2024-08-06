2c07b964b48d5675770e219a16cd70747260a254
betoab728 <betoab728@gmail.com>
2024-08-06 00:55:38 -0500

    ajuste dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -16,6 +16,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 <BrowserRouter>
    <Routes>
       <Route path="/" element={<MainLayout />}>
+         <Route index element={<Dashboard />} />
          <Route path="dashboard" element={<Dashboard />} />   
          <Route path="usuarios" element={<Usuarios />} />
          <Route path="pacientes" element={<Pacientes />} />
